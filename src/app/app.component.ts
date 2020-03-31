e2f561d9a7fda39628b530cfb197fae1bffd7c3a
sem1colon <vamsikaza@outlook.in>
2020-03-31 18:42:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
-})
-export class AppComponent  {
-  name = 'Angular';
-}
+@Component( {
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
+} )
+export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,28 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { TrackChangesAdapterComponent } from './track-changes-adapter/track-changes-adapter.component';
+import { LoadSaveIntegrationComponent } from './load-save-integration/load-save-integration.component';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { MenuComponent } from './menu/menu.component';
 
-@NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
-})
+@NgModule( {
+	declarations: [
+		AppComponent,
+		TrackChangesAdapterComponent,
+		LoadSaveIntegrationComponent,
+		MenuComponent
+	],
+	imports: [
+		BrowserModule,
+		CKEditorModule,
+		RouterModule,
+		AppRoutingModule,
+	],
+	providers: [],
+	bootstrap: [ AppComponent ]
+} )
 export class AppModule { }
